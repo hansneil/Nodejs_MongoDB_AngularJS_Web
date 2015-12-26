9fe5dac11cb8811790632ef0fa4b157ecf9fa1a1
Yu, Xianbo <yxbneil@163.com>
2015-12-26 18:06:54 +0800

    node-comment

---
+++ node-auth/config/auth.js
@@ -0,0 +1,7 @@
+module.exports = {
+  'weiboAuth' : {
+    'appkey' : 'GDdmIQH6jhtmLUypg82g',
+    'secret' : '"MCD8BKwGdgPHvAuvgvz4EQpqDAtx89grbuNMRd7Eh98',
+    'oauth_callback_url' : 'http://localhost:3005/the_dance/process_callback?service_provider_id=11'
+  }
+};
